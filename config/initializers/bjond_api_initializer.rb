7a818aa2e0d794588253c62e8c24b35b05a0f6cd
blake <blake.rego@gmail.com>
2016-07-27 18:49:52 -0400

    initializer rename.

--- config/initializers/bjond_api_initializer.rb
+++ config/initializers/bjond_api_initializer.rb
@@ -3,8 +3,8 @@ require 'bjond-api'
 integration_app = BjondIntegration::BjondAppDefinition.new
 integration_app.id           = '1e8dab76-2493-48e9-992d-fc04d6d8dabc'
 integration_app.author       = 'Bjond, Inc.'
-integration_app.name         = 'my integration app'
-integration_app.description  = 'Testing API functionality'
+integration_app.name         = 'Bjond-PokitDok-App'
+integration_app.description  = 'Adapter between Bjond and PokitDok API.'
 integration_app.iconURL      = 'https://platform.pokitdok.com/documentation/v4/images/logo.png'
 integration_app.configURL    = "http://#{Rails.application.config.action_controller.default_url_options[:host] || `hostname`}/bjond-app/services"
 integration_app.rootEndpoint = "http://#{Rails.application.config.action_controller.default_url_options[:host] || `hostname`}/bjond-app/services"
